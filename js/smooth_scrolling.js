f9b379bbbbb2fdb4cf4b44b4ef05d3cd4478b307
SarisVonLupus <62258482+SarisVonLupus@users.noreply.github.com>
2021-02-18 23:41:38 +0100

    Portfolio

---
+++ js/smooth_scrolling.js
@@ -0,0 +1,15 @@
+$('#s_header').click(function(){$('html,body').animate({
+scrollTop: $('#header').offset().top},1000);
+});
+
+$('#s_serwer_ds').click(function(){$('html,body').animate({
+scrollTop: $('#serwery').offset().top},1000);
+});
+
+$('#s_serwer_ts').click(function(){$('html,body').animate({
+scrollTop: $('#serwery').offset().top},1000);
+});
+
+$('#s_info').click(function(){$('html,body').animate({
+scrollTop: $('#info').offset().top},1000);
+});
